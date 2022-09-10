88ce3f81a7a2f1e9aa1c462859c4ec24b51542ff
jnicholz <90300060+jnicholz@users.noreply.github.com>
2022-09-09 23:52:25 -0600

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,9 +1,15 @@
+// I know you hate the "trash comments." I left in the // Console.logs because I can just do a find+replace for "// Cons" to start debugging immediately
 
 var courseSel = "";
 var courses = {};
 var stat;
 var stdID = null;
 var logIDList = [];
+var currTheme = "bg-PriSilver"
+var PriColorNodes
+var SecColorNodes
+var MainPage
+
 function createUUID() {
   return "xxxxxxx".replace(/[xy]/g, function (c) {
     var r = (Math.random() * 16) | 0,
@@ -13,15 +19,71 @@ function createUUID() {
 }
 
 async function getCourses() {
-  console.log("get Courses triggered");
-  response = await axios("/api/v1/courses");
-  for (datum in response.data) {
-    $(
-      "#course"
-    ).innerHTML += `<option value=${response.data[datum].id}>${response.data[datum].display}</option>`;
+  // console.log("get Courses triggered");
+  if(courseSel==""){
+    response = await axios("/api/v1/courses");
+    for (datum in response.data) {
+      $(
+        "#course"
+      ).innerHTML += `<option value=${response.data[datum].id}>${response.data[datum].display}</option>`;
+    }
+    courses = response.data;
   }
-  courses = response.data;
 }
+
+function checkTheme(){
+  // console.log ("theme checked")
+  PriColorNodes=document.querySelectorAll("div.bg-PriGreen")
+  SecColorNodes=document.querySelectorAll(".bg-PriSilver")
+  MainPage = document.querySelectorAll(".bg-PriWhite")  
+  console.log(`user pref: ${localStorage.theme} `)
+  console.log(`OS pref: Dark:${window.matchMedia('(prefers-color-scheme: dark)').matches} Light:${window.matchMedia('(prefers-color-scheme: light)').matches}`)
+  
+  if(localStorage.theme ){
+    setTheme(localStorage.theme)
+  }
+ 
+  //Branch here to check call of theme
+  else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    setTheme("dark")
+    console.log("OS Pref: Dark")  
+  }
+
+  else if(window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+    setTheme("light")
+    localStorage.setItem("theme", "light")
+    console.log("OS Pref: Light") 
+  }
+  else{
+    console.log("no OS Pref, Setting to Light theme")
+  }
+}
+
+function setTheme(data){
+  if (data == "dark"){
+    localStorage.setItem("theme", "dark")
+    currTheme = "bg-PriGreen"
+    modifyColors(PriColorNodes,"bg-PriGreen","bg-SecMedGrn")
+    modifyColors(SecColorNodes,"bg-PriSilver","bg-PriGreen")
+    modifyColors(MainPage,"bg-PriWhite","bg-PriBlack")
+    document.getElementById("themeToggle").setAttribute("onclick","setTheme('light')")    
+
+  }
+  else{
+    localStorage.setItem("theme", "light")
+    modifyColors(PriColorNodes,"bg-SecMedGrn","bg-PriGreen")
+    modifyColors(SecColorNodes,"bg-PriGreen","bg-PriSilver")
+    modifyColors(MainPage, "bg-PriBlack", "bg-PriWhite")
+    document.getElementById("themeToggle").setAttribute("onclick","setTheme('dark')")      
+  }
+}
+
+function modifyColors(obj, target, replacement ){
+    obj.forEach(node => {
+    node.classList.remove(target)
+    node.classList.add(replacement)
+  })
+}; 
 
 function toggleFormBody(data) {
   let form = $("#restOfBody");
@@ -53,10 +115,10 @@ async function checkID(data) {
   }
 }
 async function getNotes(id) {
-  console.log("get notes triggered");
+  // console.log("get notes triggered");
   let response = await axios({
     method: "get",
-    url: `https://json-server-jchvgz--3000.local.webcontainer.io/api/v1/logs?courseId=${courseSel}&uvuId=${id}`,
+    url: `/api/v1/logs?courseId=${courseSel}&uvuId=${id}`,
   });
   let data = response.data;
   for (datum in data) {
@@ -70,7 +132,7 @@ async function getNotes(id) {
     if (!skip) {
       $(
         "#logsList"
-      ).innerHTML += `<li onclick="toggleNote( '#${data[datum].id}')" class ="my-2 rounded bg-purple-100">
+      ).innerHTML += `<li onclick="toggleNote( '#${data[datum].id}')" class ="my-2 px-5 ${currTheme} rounded">
                   <small class="self-end"> 
                     ${data[datum].date}
                   </small>
@@ -98,7 +160,7 @@ async function sendIt() {
   try {
     let response = await axios({
       method: "post",
-      url: `https://json-server-jchvgz--3000.local.webcontainer.io/api/v1/logs?courseId=${courseSel}&uvuId=${stdID}&id=${idToUse}&courseId=${courseSel}&date=${rn.toLocaleString()}&text=${text}`,
+      url: `/api/v1/logs?courseId=${courseSel}&uvuId=${stdID}&id=${idToUse}&courseId=${courseSel}&date=${rn.toLocaleString()}&text=${text}`,
       data: {
         courseId: courseSel,
         uvuId: stdID,
@@ -107,7 +169,7 @@ async function sendIt() {
         id: idToUse,
       },
     });
-    console.log(response);
+    // console.log(response);
     getNotes(stdID);
   } catch (err) {
     console.log(err);
